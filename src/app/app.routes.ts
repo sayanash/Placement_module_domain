b9e6c62fbf89dc5bed35d12577a08455ecb0dd0a
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2024-10-15 08:50:47 +0530

    designed contactus component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './navbar/about/about.component';
 import { LearningComponent } from './navbar/learning/learning.component';
 import { LoginComponent } from './navbar/login/login.component';
 import { SignupComponent } from './navbar/signup/signup.component';
+import { ContactusComponent } from './navbar/contactus/contactus.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'learning', component: LearningComponent },
+  { path: 'contact',component:ContactusComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
