6c558aeddcbf004ec74b7ac79717078ab56d3fb8
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2024-10-16 10:00:09 +0530

    Designed Login Components

--- src/app/navbar/home/home.component.ts
+++ src/app/navbar/home/home.component.ts
@@ -6,6 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']  // Corrected to styleUrls
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {}
